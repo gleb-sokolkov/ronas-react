d12d7accebd6a3e62cf2f30508ca8bfec04d9202
gleb-sokolkov <zmihpog@gmail.com>
2022-01-26 21:00:37 +0600

    temporarly added openweatherapi mockup

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,40 @@
 import { Component } from "react";
+import { config } from "../config";
 import './app.scss';
 import { mapIcons } from "../utils/utils";
 
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      city: {
+        searchName: 'omsk',
+        name: 'Омск',
+      },
+      weather_api: process.env.REACT_APP_WEATHER_KEY,
+    };
     this.icons = mapIcons(require.context('/src/icons'), false, /\.svg$/);
+    this.getWeather = this.getWeather.bind(this);
+  }
+
+  async getWeather(city, units) {
+    let data = {};
+    try {
+      const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.state.weather_api}&units=${units}`);
+      data = await res.json();
+    } catch (err) {
+      console.error(err);
+    }
+    return data;
+  }
+
+  async componentDidMount() {
+    const weather = await this.getWeather('omsk', config.units.celcious);
+    this.setState((state) => ({ ...state, weather }));
+  }
+
+  componentDidUpdate() {
+    console.log(this.state.weather);
   }
 
   render() {
@@ -13,7 +42,7 @@ class App extends Component {
       <div className="app container">
         <nav className="main-nav app__nav">
           <div className="main-nav__title-block">
-            <h2 className="h2 main-nav__title">Омск</h2>
+            <h2 className="h2 main-nav__title">{this.state.city?.name}</h2>
             <div className="temperature-formatter main-nav__temperature-formatter secondary">
               <button className="btn temperature-formatter__item temperature-formatter__item_selected">C</button>
               <button className="btn temperature-formatter__item">F</button>
@@ -23,33 +52,46 @@ class App extends Component {
             <button className="btn location-selector__selector">Сменить город</button>
             <div className="location-selector__current">
               <img className="location-selector__icon" src={this.icons?.['location']} alt="location" />
-              <p className="location-selector__location">Моё местоположение</p>
+              <button className="btn location-selector__location">Моё местоположение</button>
             </div>
           </div>
         </nav>
         <div className="weather-display app__weather-display">
           <div className="weather-display__title">
             <img className="weather-display__icon" src={this.icons?.['sunny']} alt="location" />
-            <h1 className="weather-display__temp">19</h1>
+            <h1 className="weather-display__temp">{this.state.weather?.main.temp}</h1>
           </div>
-          <div className="weather-display__description">Преимущественно солнечно</div>
+          <div className="weather-display__description">
+            {this.state.weather?.weather[0].description}
+          </div>
         </div>
         <div className="weather-details app__weather-details">
           <div className="weather-details__item">
             <h3 className="weather-details__header secondary">Ветер</h3>
-            <p className="weather-details__data">5 м/c, западный</p>
+            <div>
+              <span className="weather-details__data weather-details__data_windspeed_ms">
+                {this.state.weather?.wind.speed}
+              </span>
+              <span className="weather-details__data">
+                {this.state.weather?.wind.direction}
+              </span>
+            </div>
           </div>
           <div className="weather-details__item">
             <h3 className="weather-details__header secondary">Давление</h3>
-            <p className="weather-details__data">752 мм рт. ст.</p>
+            <p className="weather-details__data weather-details__data_pressure_mmgh">
+              {this.state.weather?.main.pressure * 0.75}
+            </p>
           </div>
           <div className="weather-details__item">
             <h3 className="weather-details__header secondary">Влажность</h3>
-            <p className="weather-details__data">60%</p>
+            <p className="weather-details__data weather-details__data_humidity_percentage">
+              {this.state.weather?.main.humidity}
+            </p>
           </div>
           <div className="weather-details__item">
             <h3 className="weather-details__header secondary">Вероятность осадков</h3>
-            <p className="weather-details__data">10%</p>
+            <p className="weather-details__data weather-details__data_precipitation_percentage">10</p>
           </div>
         </div>
       </div>
---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const config = {
+  units: {
+    celcious: 'metric',
+    fahrenheit: 'imperial',
+  }
+};
